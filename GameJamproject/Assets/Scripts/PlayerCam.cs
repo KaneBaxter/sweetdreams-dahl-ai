bac0c68389c8ddf83ede70aa3e08670ec5b27741
KaneBaxter <kaneebaxter.business@hotmail.com>
2023-08-17 18:22:11 +0100

    PlayerMovement added
    The bean can now move, however he slides and requires speed tweaking

---
+++ GameJamproject/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX; //Controls the X axis
+    public float sensY; //Controls the Y axis
+
+    public Transform orientation;
+
+    float xRotation; // Controls the X axis rotation
+    float yRotation; // Controls the Y axis rotation
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Restricts the mouse movement to 90* angles
+
+        // Rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0 );
+
+
+    }
+
+}
